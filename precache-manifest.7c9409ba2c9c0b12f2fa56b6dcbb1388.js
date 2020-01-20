0ba191a0c644c085256414ebb50fbe9e6367a37a
mashovozka <mashovozka@gmail.com>
2020-01-20 21:20:47 +0100

    Updates

---
+++ precache-manifest.7c9409ba2c9c0b12f2fa56b6dcbb1388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcce64f8cde59b78bd8992df6dc1379",
+    "url": "/TODO-APP-useState-useEffect-useContext/index.html"
+  },
+  {
+    "revision": "c9803c5df5d18419a999",
+    "url": "/TODO-APP-useState-useEffect-useContext/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95ab296ac498a4f66f21",
+    "url": "/TODO-APP-useState-useEffect-useContext/static/js/2.9b35c58a.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/TODO-APP-useState-useEffect-useContext/static/js/2.9b35c58a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9803c5df5d18419a999",
+    "url": "/TODO-APP-useState-useEffect-useContext/static/js/main.418ee391.chunk.js"
+  },
+  {
+    "revision": "280e1e785bd6446f2a83",
+    "url": "/TODO-APP-useState-useEffect-useContext/static/js/runtime-main.119b0351.js"
+  }
+]);
